c25df0a9282c254101943ec365b18719b2a80973
Sami <xxsamixx129@gmail.com>
2025-06-27 03:37:52 +0300

    add clock in top right

--- lib/custom_widgets/my_appbar.dart
+++ lib/custom_widgets/my_appbar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:gravity_desktop_app/custom_widgets/my_text.dart';
+import 'package:gravity_desktop_app/providers/time_provider.dart';
 import 'package:gravity_desktop_app/utils/constants.dart';
 
 class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
@@ -17,6 +20,43 @@ class MyAppBar extends StatelessWidget implements PreferredSizeWidget {
         "Gravity",
         style: TextStyle(fontFamily: "Lazy Dog", fontSize: 50),
       ),
+      actions: const [
+        Padding(
+          padding: EdgeInsets.only(right: 20.0),
+          child: ClockWidget(),
+        ),
+      ],
+    );
+  }
+}
+
+class ClockWidget extends ConsumerWidget {
+  const ClockWidget({super.key});
+
+  String _formatDateTime(DateTime dateTime) {
+    // format to h:mm:ss a
+    final hour = (dateTime.hour % 12 == 0) ? 12 : dateTime.hour % 12;
+    final minute = dateTime.minute.toString().padLeft(2, '0');
+    final period = dateTime.hour < 12 ? 'AM' : 'PM';
+    return '$hour:$minute $period';
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final time = ref.watch(timeProvider);
+
+    return time.when(
+      data: (value) {
+        return Center(
+          child: Text(
+            _formatDateTime(value),
+            style:
+                AppTextStyles.sectionHeaderStyle.copyWith(color: Colors.black),
+          ),
+        );
+      },
+      loading: () => const CircularProgressIndicator(),
+      error: (error, stackTrace) => const Text('Error'),
     );
   }
 }
