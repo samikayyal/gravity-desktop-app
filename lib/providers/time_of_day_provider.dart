6e54e46d4c8aff8684d1e11677c7a4798c259b1f
Sami <xxsamixx129@gmail.com>
2025-08-01 13:41:23 +0300

    autodispose

--- lib/providers/time_of_day_provider.dart
+++ lib/providers/time_of_day_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final timeProvider = StreamProvider<DateTime>((ref) {
+final timeProvider = StreamProvider.autoDispose<DateTime>((ref) {
   return Stream.periodic(const Duration(seconds: 1), (_) => DateTime.now());
 });
